e01aac7c9e9dd59d9f140bbc78d68649ba05d2d7
Utkarsh Baghel <utkarshbaghel18@gmail.com>
2026-01-06 19:45:02 +0530

    Added delete & new views and updated DB logic

--- DB_connect/app.js
+++ DB_connect/app.js
@@ -1,18 +1,21 @@
 const { faker } = require('@faker-js/faker');
-const mysql = require('mysql2');
+const mysql = require('mysql2'); // package to connect node with mysql database
 
 const express = require('express');
 const app = express();
 const path = require('path');
 
-const methodOverride = require('method-override');
+app.use(express.urlencoded({ extended: true })); // FIRST
+
+// AFTER express.urlencoded
+const methodOverride = require('method-override'); // used to override method in form submission
 app.use(methodOverride('_method'));
 
-app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs'); // setting ejs as our view engine
+app.set("views", path.join(__dirname, "views")); // setting views directory
 
-app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "views"));
+// create connection to database
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -21,7 +24,7 @@ const connection = mysql.createConnection({
   password: '7777',
 });
 
-
+// function to generate random user data
 let getRandomUser = () => {
   return [
     faker.string.uuid(),
@@ -47,12 +50,7 @@ let getRandomUser = () => {
 //     console.error('Error executing query:', err);
 // };
 
-
 // connection.end();
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// })
 
 // this was our Home Route
 app.get('/', (req, res) => {
@@ -60,7 +58,8 @@ app.get('/', (req, res) => {
   try {
     connection.query(q, (err, results) => {
       if (err) throw err; // It immediately stops execution if an error occurs and reports the error clearly.
-      let count = results[0]['count(*)'];
+      let count = results[0]['count(*)']; // to access count value from results
+      console.log(count);
       res.render(`home.ejs`, { count });
     });
   } catch (err) {
@@ -75,7 +74,6 @@ app.get('/user', (req, res) => {
     connection.query(q, (err, results) => {
       if (err) throw err;
       let data = results;
-      console.log(data);
       res.render("show.ejs", { data });
     });
   } catch (err) {
@@ -123,6 +121,67 @@ app.patch('/user/:id', (req, res) => {
   };
 });
 
+
+// new user adding
+app.get('/user/new', (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post('/user/new', (req, res) => {
+  let { username, email, password } = req.body;
+  let id = faker.string.uuid();
+
+  let q = `insert into user (id, username, email, password) values ("${id}", "${username}", "${email}", "${password}")`;
+  try {
+    connection.query(q, (err, results) => {
+      if (err) throw err;
+      res.redirect('/user');
+    });
+  } catch (err) {
+    console.error('Error executing query:', err);
+  };
+});
+
+// Delete route
+app.get('/user/:id/delete', (req, res) => {
+  let { id } = req.params;
+  let q = `select * from user where id = "${id}"`; // to add id as string we have to use ""
+  try {
+    connection.query(q, (err, results) => {
+      if (err) throw err;
+      let user = results[0]; // since we are getting only one user we take the first element of array means our whole data of that user
+      res.render("delete.ejs", { user });
+    });
+  } catch (err) {
+    console.error('Error executing query:', err);
+  };
+});
+app.delete('/user/:id', (req, res) => {
+  let { id } = req.params;
+  let { password: formpass, username: newusername } = req.body;
+  let q = `select * from user where id = "${id}"`;
+  try {
+    connection.query(q, (err, results) => {
+      if (err) throw err;
+      let userpass = results[0].password;
+      if (formpass != userpass) {
+        return res.send("Wrong Password! Cannot update username.");
+      }
+      else {
+        let q2 = `delete from user where id = "${id}"`;
+        connection.query(q2, (err, results) => {
+          if (err) throw err;
+          res.redirect('/user');
+        });
+      }
+    });
+  } catch (err) {
+    console.error('Error executing query:', err);
+  };
+});
+
+
+// starting the server
 app.listen(7700, () => {
   console.log('Server is running on port 7700');
 });

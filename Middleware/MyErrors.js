87efde086c35d96031c404f99b98c321696e1ef7
Utkarsh Baghel <utkarshbaghel18@gmail.com>
2026-01-10 17:49:08 +0530

    Adding Custom Errors

---
+++ Middleware/MyErrors.js
@@ -0,0 +1,8 @@
+class MyErrors extends Error {
+    constructor(status , message){
+        super();
+        this.status = status;
+        this.message = message;
+    }
+};
+module.exports = MyErrors;
--- Middleware/app.js
+++ Middleware/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const MyErrors = require('./MyErrors');
+
 // Custom middleware function   
 app.use('/api' , (req, res, next) => {
     let {token} = req.query;
     if(token === '7777'){
         next();
     }
-    throw new Error('Invalid token provided'); // this is how errors are thrown in express. And we can make our own custom errors like this also. 
+    throw new MyErrors(477, 'Invalid token provided'); // this is how errors are thrown in express. And we can make our own custom errors like this in MyErrors.js . 
 });
 
 // Sample route
@@ -21,6 +23,18 @@ app.get('/api', (req, res) => {
 
 app.get('/api/data', (req, res) => {
     res.send("Data shown here.");
+});
+
+app.get('/admin' , (req, res) => {
+    if(req.query.admin === 'true'){
+        return res.send("Welcome Admin");
+    }
+    throw new MyErrors(403,'Access to Admin is forbidden');
+});
+
+app.use((err, req, res, next) => {
+    console.log("---ERROR---");
+    res.send(err);
 });
 
 app.listen(port, () => {
